218b2fc6ef41d9a0bb063a21861326fb5c29b1cb
elen <elenndev@gmail.com>
2025-08-26 21:49:16 -0300

    update ai context about some location info

--- src/service/gemini.ts
+++ src/service/gemini.ts
@@ -21,7 +21,7 @@ export async function askGemini(question: string) {
       ${question}
       ---
 
-      **Instrução:** Com base APENAS no contexto fornecido, responda à pergunta do usuário. Se a resposta não estiver no contexto, diga que não tem as informações para responder a aquela pergunta, ao responder, não responda como 'Com base no contexto fornecido, a resposta é...' e sim de uma forma mais amigável que soe mais natural para quem fez a pergunta.
+      **Instrução:** Com base APENAS no contexto fornecido, responda à pergunta do usuário. Se a resposta não estiver no contexto, diga que não tem as informações para responder a aquela pergunta, ao responder, não responda como 'Com base no contexto fornecido, a resposta é...' ou 'pelo que o texto diz...' e sim de uma forma mais amigável que soe mais natural para quem fez a pergunta, não faça o usuário ter noção de que existe um texto que você está seguindo, apenas responda a pergunta com as informações fornecidas e caso não seja possível responder, responda que não tem as informações necessárias para responder.
     `,
   });
 
